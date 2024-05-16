90c80ab2167f59171e9e643e991bf419b68f7ed8
Mahedere <mahederetebebe98@gmail.com>
2024-05-16 14:10:17 -0700

    Balance comment added

--- budget_tracker/src/components/Balance.js
+++ budget_tracker/src/components/Balance.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { GlobalContext } from '../GlobalState';
 function Balance() {
-    const { transactions } = useContext(GlobalContext);
-  const amounts = transactions.map(transaction => transaction.amount);
+    const { transactions } = useContext(GlobalContext); //to access the context
+  const amounts = transactions.map(transaction => transaction.amount);//for each transaction extracts the amount property
 
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2); //rounding the final result to 2 decimal points
   return (
     <div>
       <h4>Your Balance</h4>
