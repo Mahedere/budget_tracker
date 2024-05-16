6e41e382a49349c68a970401815810f233515d03
Mahedere <mahederetebebe98@gmail.com>
2024-05-16 14:04:51 -0700

    balance.js updated

--- budget_tracker/src/components/Balance.js
+++ budget_tracker/src/components/Balance.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function Balance() {
+    const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div>
       <h4>Your Balance</h4>
