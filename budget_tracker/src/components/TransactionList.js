4363ed021145a6cfcb89d14cca20b0148b13b643
Mahedere <mahederetebebe98@gmail.com>
2024-05-16 13:49:06 -0700

    transactionlist comment added

--- budget_tracker/src/components/TransactionList.js
+++ budget_tracker/src/components/TransactionList.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../GlobalState';
+import React, { useContext } from 'react'; //imports usecontext which allows to access context value
+import { GlobalContext } from '../GlobalState'; //imports globalcontext provides the globalstate for the app
 
 function TransactionList() {
   const { transactions, deleteTransaction } = useContext(GlobalContext);
@@ -10,7 +10,7 @@ function TransactionList() {
       <ul className="list">
         {transactions.map(transaction => (
           <li className={transaction.amount < 0 ? 'minus' : 'plus'} key={transaction.id}>
-            {transaction.text} <span>{transaction.amount < 0 ? '-' : '+'}${Math.abs(transaction.amount)}</span>
+            {transaction.text} <span>{transaction.amount < 0 ? '-' : '+'}${Math.abs(transaction.amount)}</span>{/*math ensures that the absolute value is displayed */}
             <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
           </li>
         ))}
