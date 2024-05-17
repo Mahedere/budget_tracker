d34a764705959b87a0bf077a4011453d663137d3
Mahedere <mahederetebebe98@gmail.com>
2024-05-17 08:05:41 -0700

    Header.js updated

--- budget_tracker/src/App.js
+++ budget_tracker/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 function App() {
   return (
     <div className="App">
       <h1>Budget Tracker</h1>
+      <Header/>
+      <Balance/>
+      <IncomeExpenses/>
+      <AddTransaction/>
+      <TransactionList/>
     </div>
   );
 }
--- budget_tracker/src/components/Header.js
+++ budget_tracker/src/components/Header.js
@@ -9,3 +9,4 @@ function Header(){
         </Navbar>
     );
 }
+export default Header;
